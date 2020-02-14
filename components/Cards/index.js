745552947a6e065937cd931484f2415c8a1ab256
David Shestopal <dshestopal@yahoo.com>
2020-02-14 09:22:42 -0800

    mvp

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+    console.log(response);
+    let cardContainer = document.querySelector('.cards-container');
+    let carddata = Object.values(response.data.articles);
+    carddata.forEach(element => {
+      element.forEach(item => {
+        cardContainer.appendChild(cardMaker(item));
+      });
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+const cardMaker = response => {
+  const newCard = document.createElement('div');
+  newCard.classList.add('card');
+
+  const newtitle = document.createElement('div');
+  newtitle.classList.add('headline');
+  newtitle.textContent = response.headline;
+  newCard.appendChild(newtitle);
+
+  const newAuthor = document.createElement('div');
+  newAuthor.classList.add('author');
+  newCard.appendChild(newAuthor);
+
+  const newImg = document.createElement('div');
+  newImg.classList.add('img-container');
+  newAuthor.appendChild(newImg);
+
+  const newImageLink = document.createElement('img');
+  newImageLink.setAttribute('src', response.authorPhoto);
+  newImg.appendChild(newImageLink);
+
+  const newWriter = document.createElement('span');
+  newWriter.textContent = `By ${response.authorName}`;
+  newAuthor.appendChild(newWriter);
+
+  return newCard;
+};
+console.log(cardMaker);
